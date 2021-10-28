fcaaccf119e17b0d4d1bc4b25d4734875655cb0b
Andre Lussier <Dremon7@hotmail.com>
2021-10-27 21:47:22 -0400

    EchoServer implemented

--- src/EchoServer.java
+++ src/EchoServer.java
@@ -1,2 +1,31 @@
+import com.sun.security.ntlm.Server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class EchoServer {
+    ServerSocket serverSocket;
+    Socket clientSocket;
+    PrintWriter out;
+    BufferedReader in;
+
+    public void start(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        clientSocket = serverSocket.accept();
+        out = new PrintWriter(clientSocket.getOutputStream(),true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        String inputLine;
+        while((inputLine = in.readLine()) != null){
+            if(".".equals(inputLine)){
+                out.println("good bye");
+                break;
+            }
+            out.println(inputLine);
+        }
+    }
 }
